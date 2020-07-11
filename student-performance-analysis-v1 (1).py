9bfc77c4b9302fff8eb77625477cb3184a4104ea
matout7 <55733922+matout7@users.noreply.github.com>
2020-07-11 13:59:04 +0300

    Update student-performance-analysis-v1 (1).py
    Minor Cosmetic Edits

--- student-performance-analysis-v1 (1).py
+++ student-performance-analysis-v1 (1).py
@@ -1,85 +1,47 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Student Performance Analysis and Visualizations
-
-# <img src="https://i2-prod.mirror.co.uk/incoming/article4479481.ece/ALTERNATES/s615/A-level-students-sit-an-A-level-maths-exam-inside-a-sports-hall.jpg" width="800px">
-
-# In[1]:
-
-
 get_ipython().system('pip install dabl')
 
 
-# In[2]:
-
-
-# for some basic operations
+# basic operations
 import numpy as np
 import pandas as pd
 
-# for visualizations
+# visualizations
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 
 import dabl
 
-# for providing path
+# providing path
 import os
 print(os.listdir('../input/'))
 
 
-# **Reading the Data set**
-
-# In[3]:
-
-
-# reading the data
+# reading data
 data = pd.read_csv('../input/StudentsPerformance.csv')
 
 # getting the shape of the data
 print(data.shape)
-
-
-# In[4]:
-
 
 # looking at the head of the data
 
 data.head()
 
 
-# ## Descriptive Statistics
-
-# In[5]:
-
-
+# Descriptive Statistics
 # describing the dataset
 
 data.describe()
 
-
-# In[6]:
-
-
-# lets check the no. of unique items present in the categorical column
+# No. of unique items present in the categorical column
 
 data.select_dtypes('object').nunique()
 
-
-# In[7]:
-
-
-# lets check the percentage of missing data in each columns present in the data
+# Percentage of missing data in each columns present in the data
 
 no_of_columns = data.shape[0]
 percentage_of_missing_data = data.isnull().sum()/no_of_columns
 print(percentage_of_missing_data)
-
-
-# In[8]:
-
 
 # comparison of all other attributes with respect to Math Marks
 
@@ -87,19 +49,11 @@ plt.rcParams['figure.figsize'] = (18, 6)
 plt.style.use('fivethirtyeight')
 dabl.plot(data, target_col = 'math score')
 
-
-# In[9]:
-
-
 # comparison of all other attributes with respect to Reading Marks
 
 plt.rcParams['figure.figsize'] = (18, 6)
 plt.style.use('fivethirtyeight')
 dabl.plot(data, target_col = 'reading score')
-
-
-# In[10]:
-
 
 # comparison of all other attributes with respect to Writing Marks
 
@@ -107,13 +61,9 @@ plt.rcParams['figure.figsize'] = (18, 6)
 plt.style.use('fivethirtyeight')
 dabl.plot(data, target_col = 'writing score')
 
+# Inferential Statistics
 
-# ## Inferential Statistics
-
-# ** Lets check the Probability of Students Scoring More than 50 Marks in Maths**
-
-# In[11]:
-
+# Probability of Students Scoring More than 50 Marks in Maths
 
 total_students = data.shape[0]
 students_score_more_than_50 = data[data['math score'] > 50].shape[0]
@@ -121,19 +71,11 @@ students_score_more_than_50 = data[data['math score'] > 50].shape[0]
 probability_of_students_scoring_more_than_50_in_maths = (students_score_more_than_50/total_students)*100
 print("Probability of Students Scoring more than 50 marks in Maths :", probability_of_students_scoring_more_than_50_in_maths)
 
-
-# In[12]:
-
-
 total_students = data.shape[0]
 students_score_more_than_50 = data[data['reading score'] > 50].shape[0]
 
 probability_of_students_scoring_more_than_50_in_reading = (students_score_more_than_50/total_students)*100
 print("Probability of Students Scoring more than 50 marks in Reading :", probability_of_students_scoring_more_than_50_in_reading)
-
-
-# In[13]:
-
 
 total_students = data.shape[0]
 students_score_more_than_50 = data[data['writing score'] > 50].shape[0]
@@ -142,10 +84,7 @@ probability_of_students_scoring_more_than_50_in_writing = (students_score_more_t
 print("Probability of Students Scoring more than 50 marks in Writing :", probability_of_students_scoring_more_than_50_in_writing)
 
 
-# ** Lets also check the Probability of Students Passing in all the three Subjects**
-
-# In[14]:
-
+# Probability of Students Passing in all the three Subjects
 
 total_students = data.shape[0]
 number_of_students_passing_in_all_subjects = data[(data['math score'] > 40) &
@@ -155,10 +94,7 @@ probability_of_students_passing_in_all_the_subjects = (number_of_students_passin
 print("The Probability of Students Passing in all the Subjects is {0:.2f} %".format(probability_of_students_passing_in_all_the_subjects))
 
 
-# ** Lets also check the Probability of Students Scoring more than 90 in all the three Subjects**
-
-# In[15]:
-
+# Probability of Students Scoring more than 90 in all the three Subjects
 
 total_students = data.shape[0]
 number_of_students_scoring_more_than_90 = data[(data['math score'] > 90) &
@@ -170,11 +106,7 @@ print("The Probability of Students Passing in all the Subjects is {0:.2f} %".
       format(probability_of_students_scoring_more_than_90_in_all_subjects))
 
 
-# ** Checking for Skewness for the Maths, Reading and Writing Scores**
-# 
-
-# In[16]:
-
+# Skewness for the Maths, Reading and Writing Scores
 
 plt.subplot(1, 3, 1)
 sns.distplot(data['math score'])
@@ -191,55 +123,49 @@ plt.show()
 
 # * It is very much clear, that there is no skewness in the Target Columns, 
 
-# **Lets check the Inference**
+# **check the Inference**
 
-# In[17]:
-
-
-# lets take seed so that everytime the random values come out to be constant
+# Take seed so that everytime the random values come out to be constant
 np.random.seed(6)
 
-# lets take 100 sample values from the dataset of 1000 values
+# Take 100 sample values from the dataset of 1000 values
 sample_math_marks = np.random.choice(a= data['math score'], size=100)
 
-# getting the sample mean
+# sample mean
 print ("Sample mean for Math Scores:", sample_math_marks.mean() )          
 
-# getting the population mean
+# population mean
 print("Population mean for Math Scores:", data['math score'].mean())
 
-# lets take 100 sample values from the dataset of 1000 values
+# Take 100 sample values from the dataset of 1000 values
 sample_reading_marks = np.random.choice(a= data['reading score'], size=100)
 
-# getting the sample mean
+# sample mean
 print ("\nSample mean for Reading Scores:", sample_reading_marks.mean() )          
 
-# getting the population mean
+# population mean
 print("Population mean for Reading Scores:", data['reading score'].mean())
 
-# lets take 100 sample values from the dataset of 1000 values
+# Take 100 sample values from the dataset of 1000 values
 sample_writing_marks = np.random.choice(a= data['writing score'], size=100)
 
-# getting the sample mean
+# sample mean
 print ("\nSample mean for Writing Scores:", sample_math_marks.mean() )          
 
-# getting the population mean
+# population mean
 print("Population mean for Writing Scores:", data['writing score'].mean())
 
 
-# ** Let check the Confidence Interval for Math Score**
+# ** Confidence Interval for Math Score**
 
-# In[18]:
-
-
-# lets import the scipy package
+# Import the scipy package
 import scipy.stats as stats
 import math
 
-# lets seed the random values
+# Seed the random values
 np.random.seed(10)
 
-# lets take a sample size
+# take a sample size
 sample_size = 1000
 sample = np.random.choice(a= data['math score'],
                           size = sample_size)
@@ -251,7 +177,7 @@ z_critical = stats.norm.ppf(q = 0.95)
  # Check the z-critical value  
 print("z-critical value: ",z_critical)                                
 
-# Get the population standard deviation
+# population standard deviation
 pop_stdev = data['math score'].std()  
 
 # checking the margin of error
@@ -261,56 +187,50 @@ margin_of_error = z_critical * (pop_stdev/math.sqrt(sample_size))
 confidence_interval = (sample_mean - margin_of_error,
                        sample_mean + margin_of_error)  
 
-# lets print the results
+# print the results
 print("Confidence interval:",end=" ")
 print(confidence_interval)
 print("True mean: {}".format(data['math score'].mean()))
 
 
-# ** Let check the Confidence Interval for Reading Score**
+# ** Check the Confidence Interval for Reading Score**
 
-# In[19]:
-
-
-# lets import the scipy package
+# import the scipy package
 import scipy.stats as stats
 import math
 
-# lets seed the random values
+# seed the random values
 np.random.seed(10)
 
-# lets take a sample size
+# sample size
 sample_size = 1000
 sample = np.random.choice(a= data['reading score'],
                           size = sample_size)
 sample_mean = sample.mean()
 
-# Get the z-critical value*
+# z-critical value*
 z_critical = stats.norm.ppf(q = 0.95)  
 
- # Check the z-critical value  
+ # z-critical value  
 print("z-critical value: ",z_critical)                                
 
-# Get the population standard deviation
+# population standard deviation
 pop_stdev = data['reading score'].std()  
 
-# checking the margin of error
+# margin of error
 margin_of_error = z_critical * (pop_stdev/math.sqrt(sample_size)) 
 
 # defining our confidence interval
 confidence_interval = (sample_mean - margin_of_error,
                        sample_mean + margin_of_error)  
 
-# lets print the results
+# print the results
 print("Confidence interval:",end=" ")
 print(confidence_interval)
 print("True mean: {}".format(data['reading score'].mean()))
 
 
 # ** Let check the Confidence Interval for Writing Score**
-
-# In[20]:
-
 
 # lets seed the random values
 np.random.seed(10)
@@ -351,9 +271,6 @@ print("True mean: {}".format(data['writing score'].mean()))
 
 # **Number of Girl Students Scoring 90 in all the Subjects**
 
-# In[21]:
-
-
 data[(data['gender'] == 'female') &
      (data['math score'] > 90) & 
      (data['writing score'] > 90) &
@@ -362,24 +279,15 @@ data[(data['gender'] == 'female') &
 
 # **Lets compare the scores secured by Boys and Girls**
 
-# In[22]:
-
-
 data.groupby(['gender']).agg(['min','median','max'])
 
 
 # **Lets check the Effect of Lunch on Student's Performnce**
 
-# In[23]:
-
-
 data[['lunch','gender','math score','writing score','reading score']].groupby(['lunch','gender']).agg('median')
 
 
 # **Lets check the Effect of Test Preparation Course on Scores**
-
-# In[24]:
-
 
 data[['test preparation course',
       'gender',
@@ -390,9 +298,6 @@ data[['test preparation course',
 
 # **Lets check the Effect of Race and Ethnicity on Student's Performance**
 
-# In[25]:
-
-
 data[['race/ethnicity',
       'math score',
       'writing score',
@@ -400,9 +305,6 @@ data[['race/ethnicity',
 
 
 # ##  Data Visualizations
-
-# In[26]:
-
 
 # visualising the number of male and female in the dataset
 
@@ -413,10 +315,6 @@ plt.title('Comparison of Males and Females', fontweight = 30)
 plt.xlabel('Gender')
 plt.ylabel('Count')
 plt.show()
-
-
-# In[27]:
-
 
 # visualizing the different groups in the dataset
 
@@ -429,10 +327,6 @@ plt.xlabel('Groups')
 plt.ylabel('count')
 plt.show()
 
-
-# In[28]:
-
-
 # visualizing the differnt parental education levels
 
 plt.rcParams['figure.figsize'] = (15, 9)
@@ -444,10 +338,6 @@ plt.xlabel('Degree')
 plt.ylabel('count')
 plt.show()
 
-
-# In[29]:
-
-
 # visualizing different types of lunch 
 
 plt.rcParams['figure.figsize'] = (15, 9)
@@ -458,10 +348,6 @@ plt.title('Comparison of different types of lunch', fontweight = 30, fontsize = 
 plt.xlabel('types of lunch')
 plt.ylabel('count')
 plt.show()
-
-
-# In[30]:
-
 
 # visualizing maths score
 
@@ -475,10 +361,6 @@ plt.ylabel('count')
 plt.xticks(rotation = 90)
 plt.show()
 
-
-# In[31]:
-
-
 ## visualizing reading score
 
 plt.rcParams['figure.figsize'] = (15, 9)
@@ -490,10 +372,6 @@ plt.xlabel('score')
 plt.ylabel('count')
 plt.xticks(rotation = 90)
 plt.show()
-
-
-# In[32]:
-
 
 # visualizing writing score
 
@@ -507,10 +385,6 @@ plt.ylabel('count')
 plt.xticks(rotation = 90)
 plt.show()
 
-
-# In[33]:
-
-
 # gender vs race/etnicity 
 
 plt.rcParams['figure.figsize'] = (15, 9)
@@ -518,10 +392,6 @@ x = pd.crosstab(data['gender'], data['race/ethnicity'])
 x.div(x.sum(1).astype(float), axis = 0).plot(kind = 'bar', stacked = False)
 plt.title('Gender vs Race', fontweight = 30, fontsize = 20)
 plt.show()
-
-
-# In[34]:
-
 
 # comparison of race/ethnicity and parental level of education
 
@@ -531,10 +401,6 @@ x.div(x.sum(1).astype(float), axis = 0).plot(kind = 'bar', stacked = 'True')
 plt.title('Race vs Parental Education', fontweight = 30, fontsize = 20)
 plt.show()
 
-
-# In[35]:
-
-
 # comparison of parental degree and test course
 
 plt.rcParams['figure.figsize'] = (15, 9)
@@ -542,19 +408,11 @@ sns.countplot(x = 'parental level of education', data = data, hue = 'test prepar
 plt.title('Parental Education vs Test Preparation Course', fontweight = 30, fontsize = 20)
 plt.show()
 
-
-# In[36]:
-
-
 # comparison of race/ethnicity and test preparation course
 
 sns.countplot(x = 'race/ethnicity', data = data,  hue = 'test preparation course', palette = 'bright')
 plt.title('Race vs Test Preparion', fontweight = 30, fontsize = 20)
 plt.show()
-
-
-# In[37]:
-
 
 # feature engineering on the data to visualize and solve the dataset more accurately
 
@@ -570,10 +428,6 @@ plt.xlabel('status')
 plt.ylabel('count')
 plt.show()
 
-
-# In[38]:
-
-
 # creating a new column pass_reading, this column will tell us whether the students are pass or fail
 
 data['pass_reading'] = np.where(data['reading score']< passmarks, 'Fail', 'Pass')
@@ -584,10 +438,6 @@ plt.xlabel('status')
 plt.ylabel('count')
 plt.show()
 
-
-# In[39]:
-
-
 # creating a new column pass_writing, this column will tell us whether the students are pass or fail
 
 data['pass_writing'] = np.where(data['writing score']< passmarks, 'Fail', 'Pass')
@@ -597,10 +447,6 @@ plt.title('Pass/Fail in Writing', fontweight = 30, fontsize = 20)
 plt.xlabel('status')
 plt.ylabel('count')
 plt.show()
-
-
-# In[40]:
-
 
 # computing the total score for each student
 
@@ -615,11 +461,6 @@ plt.title('comparison of total score of all the students', fontweight = 30, font
 plt.xlabel('total score scored by the students')
 plt.ylabel('count')
 plt.show()
-
-
-# In[41]:
-
-
 
 # computing percentage for each of the students
 # importing math library to use ceil
@@ -640,10 +481,6 @@ plt.xlabel('Percentage scored')
 plt.ylabel('Count')
 plt.show()
 
-
-# In[42]:
-
-
 # checking which student is fail overall
 
 data['status'] = data.apply(lambda x : 'Fail' if x['pass_math'] == 'Fail' or 
@@ -655,10 +492,6 @@ plt.title('overall results', fontweight = 30, fontsize = 20)
 plt.xlabel('status')
 plt.ylabel('count')
 plt.show()
-
-
-# In[43]:
-
 
 # Assigning grades to the grades according to the following criteria :
 # 0  - 40 marks : grade E
@@ -689,9 +522,6 @@ data['grades'] = data.apply(lambda x: getgrade(x['percentage'], x['status']), ax
 data['grades'].value_counts()
 
 
-# In[44]:
-
-
 # plotting a pie chart for the distribution of various grades amongst the students
 
 labels = ['Grade 0', 'Grade A', 'Grade B', 'Grade C', 'Grade D', 'Grade E']
@@ -707,9 +537,6 @@ plt.tight_layout()
 plt.show()
 
 
-# In[45]:
-
-
 # comparison parent's degree and their corresponding grades
 
 plt.rcParams['figure.figsize'] = (15, 9)
@@ -720,19 +547,11 @@ x.div(x.sum(1).astype(float), axis = 0).plot(kind = 'bar', stacked = True, color
 plt.title("Parental Education vs Student's Grades", fontweight = 30, fontsize = 20)
 plt.show()
 
-
-# In[46]:
-
-
 # for better visualization we will plot it again using seaborn
 
 sns.countplot(x = data['parental level of education'], data = data, hue = data['grades'], palette = 'pastel')
 plt.title('Parental Education vs Grades of Students', fontsize = 20, fontweight = 30)
 plt.show()
-
-
-# In[47]:
-
 
 # comparing the distribution of grades among males and females
 
@@ -743,9 +562,6 @@ plt.show()
 
 
 # ## Label Encoding
-
-# In[48]:
-
 
 from sklearn.preprocessing import LabelEncoder
 
@@ -787,9 +603,6 @@ data['status'] = le.fit_transform(data['status'])
 
 # ## Data Preparation
 
-# In[49]:
-
-
 # splitting the dependent and independent variables
 
 x = data.iloc[:,:14]
@@ -797,10 +610,6 @@ y = data.iloc[:,14]
 
 print(x.shape)
 print(y.shape)
-
-
-# In[50]:
-
 
 # splitting the dataset into training and test sets
 
@@ -813,10 +622,6 @@ print(y_train.shape)
 print(x_test.shape)
 print(y_test.shape)
 
-
-# In[51]:
-
-
 # importing the MinMaxScaler
 from sklearn.preprocessing import MinMaxScaler
 
@@ -826,10 +631,6 @@ mm = MinMaxScaler()
 # feeding the independent variable into the scaler
 x_train = mm.fit_transform(x_train)
 x_test = mm.transform(x_test)
-
-
-# In[52]:
-
 
 # applying principal components analysis
 
@@ -858,9 +659,6 @@ from sklearn.decomposition import PCA
 
 # ## Logistic Regression
 
-# In[53]:
-
-
 from sklearn.linear_model import  LogisticRegression
 
 # creating a model
@@ -875,10 +673,6 @@ y_pred = model.predict(x_test)
 # calculating the classification accuracies
 print("Training Accuracy :", model.score(x_train, y_train))
 print("Testing Accuracy :", model.score(x_test, y_test))
-
-
-# In[54]:
-
 
 # printing the confusion matrix
 
@@ -896,9 +690,6 @@ plt.show()
 
 # **Random Forest**
 
-# In[55]:
-
-
 from sklearn.ensemble import RandomForestClassifier
 
 # creating a model
@@ -914,10 +705,6 @@ y_pred = model.predict(x_test)
 print("Training Accuracy :", model.score(x_train, y_train))
 print("Testing Accuracy :", model.score(x_test, y_test))
 
-
-# In[56]:
-
-
 # printing the confusion matrix
 
 from sklearn.metrics import confusion_matrix
@@ -931,15 +718,7 @@ sns.heatmap(cm, annot = True, cmap = 'Reds')
 plt.title('Confusion Matrix for Random Forest', fontweight = 30, fontsize = 20)
 plt.show()
 
-
-# In[57]:
-
-
 get_ipython().system('pip install yellowbrick')
-
-
-# In[58]:
-
 
 from pandas.plotting import radviz
 fig, ax = plt.subplots(figsize=(12, 12))
@@ -951,166 +730,3 @@ plt.show()
 
 
 # * It gives a clear Idea that Students getting very low grades have high correlation on Lunch and Parental Education
-
-# In[ ]:
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
